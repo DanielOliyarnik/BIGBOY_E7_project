8dfa258b357a50a14e5b88446ffdb0c376c3c387
DanielOliyarnik <81031004+DanielOliyarnik@users.noreply.github.com>
2023-07-15 15:25:43 -0400

    fixd broken stuff

--- stuff/stuff.ino
+++ stuff/stuff.ino
@@ -2,8 +2,10 @@
 #include <LiquidCrystal_I2C.h>
 #include <EEPROM.h>
 #include "GravityTDS.h"
+#include <Servo.h>
 
 #define MotorInterfaceType 4
+#define TdsSensorPin A1
 
 AccelStepper stepper_left(MotorInterfaceType, 4, 5, 6, 7); // Defaults right wheel to (4 pins) on 4, 5, 6, 7
 AccelStepper stepper_right(MotorInterfaceType, 10, 11, 12, 13); // Defaults left wheel to (4 pins) on 10, 11, 12, 13
@@ -11,7 +13,10 @@ AccelStepper stepper_right(MotorInterfaceType, 10, 11, 12, 13); // Defaults left
 LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
 
 Servo servo;
+GravityTDS gravityTds;
 int angle = 0;
+int tdsValue = 0;
+float temperature = 25;
 
 void setup() {
   // Setup for sensor arm servo
@@ -33,16 +38,16 @@ void loop() {
   straight(11350+525);
   //dip
   straight(1025);
-  right_turn();
-  straight();
-  left_turn();
-  straight();
-  //dip
-  straight():
-  left_turn();
-  straight();
-  right_turn();
-  straight();
+  // right_turn();
+  // straight();
+  // left_turn();
+  // straight();
+  // sample_dip();
+  // straight():
+  // left_turn();
+  // straight();
+  // right_turn();
+  // straight();
 
 }
 
@@ -57,14 +62,25 @@ int sample() {
     return tdsValue;
 }
 
+void LCD_Display(int TDS_value) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(TDS_value + " mg/L");
+  lcd.setCursor(0, 2);
+  if(TDS_value>=600) {
+	  lcd.print("UNSAFE!");
+  } else {
+	  lcd.print("SAFE");
+  }
+}
 //assuming to start at 0 degrees
 void sample_dip() {
   for (angle = 10; angle < 90; angle++){
     servo.write(angle);               
     delay(15);    
   }
-  reading = sample();
-  for (angle; angle > 0; angle--){
+  int reading = sample();
+  for (angle= 90; angle > 0; angle--){
     servo.write(angle);               
     delay(15);    
   }
@@ -72,7 +88,7 @@ void sample_dip() {
   delay(1000);
 }
 
-void straight (ticks) {
+void straight (int ticks) {
 	stepper_left.move(ticks);
 	stepper_left.setMaxSpeed(500);
 	stepper_left.setAcceleration(100);
@@ -84,32 +100,19 @@ void straight (ticks) {
   stepper_right.run();
 }
 
-void left_turn (ticks) {
+void left_turn (int ticks) {
 	stepper_left.move(ticks);
 	stepper_left.setMaxSpeed(500);
 	stepper_left.setAcceleration(100);
   stepper_left.run();
 }
 
-void right_turn (ticks) {
+void right_turn (int ticks) {
 	stepper_right.move(ticks);
 	stepper_right.setMaxSpeed(500);
 	stepper_right.setAcceleration(100);
   stepper_right.run();
 }
 
-void LCD_Display(TDS_value) {
-
-  lcd.clear();                 // clear display
-  lcd.setCursor(0, 0);         // move cursor to   (0, 0)
-  lcd.print(TDS_value + " mg/L");        // print message at (0, 0)
-  lcd.setCursor(0, 2);         // move cursor to   (0, 2)
-
-  if(TDS_value>=600) {
-	  lcd.print("UNSAFE!");        // print message at (0, 2)
-  } else {
-	  lcd.print("SAFE");
-  }
-
   //delay(2000);                 // display the above for two seconds
-}
+
